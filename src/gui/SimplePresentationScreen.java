08ae822d9bb45c430cace37dfb72d9da63de2323
Ezequiel <ezequielmaximiliano123@gmail.com>
2022-08-28 16:52:27 -0300

    final

--- src/gui/SimplePresentationScreen.java
+++ src/gui/SimplePresentationScreen.java
@@ -1,14 +1,18 @@
 package gui;
-
+import java.util.*;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 import entities.Student;
-import javax.swing.JTabbedPane;
+
+
+
 
 @SuppressWarnings("serial")
 public class SimplePresentationScreen extends JFrame {
@@ -17,8 +21,14 @@ public class SimplePresentationScreen extends JFrame {
 	private JPanel tabInformation;
 	private JTabbedPane tabbedPane;
 	private Student studentData;
+	private JLabel labelLu;
+	
+	private JLabel textoFecha;
+	private JLabel labelFecha;
+	private JLabel labelHora;
 
 	public SimplePresentationScreen(Student studentData) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(SimplePresentationScreen.class.getResource("/images/tdp.png")));
 		this.studentData = studentData;
 		
 		contentPane = new JPanel();
@@ -39,7 +49,96 @@ public class SimplePresentationScreen extends JFrame {
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabInformation = new JPanel();
 		tabInformation.setPreferredSize(new Dimension(425, 275));
-		tabbedPane.addTab("InformaciÃ³n del alumno", null, tabInformation, "Muestra la informaciÃ³n declarada por el alumno");
+		tabbedPane.addTab("Información del alumno", null, tabInformation, "Muestra la información declarada por el alumno");
+		
+		JLabel labelImagen = new JLabel("");
+		labelImagen.setBounds(251, 21, 142, 90);
+		tabInformation.add(labelImagen);
+		
+		ImageIcon image = new ImageIcon(studentData.getPathPhoto());
+	    Icon icon = new ImageIcon(image.getImage().getScaledInstance(labelImagen.getWidth(), labelImagen.getHeight(), Image.SCALE_DEFAULT));
+	    labelImagen.setIcon(icon);
+	    
+		tabInformation.setLayout(null);
+		
+		Calendar fecha = new GregorianCalendar();
+		String anho = Integer.toString(fecha.get(Calendar.YEAR));
+		String mes = Integer.toString(fecha.get(Calendar.MONTH)+1);
+		String dia = Integer.toString(fecha.get(Calendar.DATE));
+		String fechaCompleta = anho+"/"+mes+"/"+dia;
+		String minute = "";
+		if (fecha.get(Calendar.MINUTE)<10) {
+			minute = "0"+Integer.toString(fecha.get(Calendar.MINUTE));
+		}
+		else {
+			minute = Integer.toString(fecha.get(Calendar.MINUTE));
+		}
+		String hora = Integer.toString(fecha.get(Calendar.HOUR_OF_DAY));
+		String horaComp = hora+":"+minute;
+		
+		labelLu = new JLabel("LU");
+		labelLu.setBounds(10, 11, 49, 14);
+		tabInformation.add(labelLu);
+		
+		JLabel LabelApellido = new JLabel("Apellido");
+		LabelApellido.setBounds(10, 46, 49, 14);
+		tabInformation.add(LabelApellido);
+		
+		JLabel LabelNombre = new JLabel("Nombre");
+		LabelNombre.setBounds(10, 85, 49, 14);
+		tabInformation.add(LabelNombre);
+		
+		JLabel LabelMail = new JLabel("E-mail");
+		LabelMail.setBounds(10, 119, 49, 14);
+		tabInformation.add(LabelMail);
+		
+		JLabel LabelGit = new JLabel("Github");
+		LabelGit.setBounds(10, 150, 67, 14);
+		tabInformation.add(LabelGit);
+		
+		
+		
+		textoFecha = new JLabel("Esta ventana se genero el ");
+		textoFecha.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		textoFecha.setBounds(234, 151, 113, 14);
+		tabInformation.add(textoFecha);
+		
+		labelFecha = new JLabel(fechaCompleta);
+		labelFecha.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		labelFecha.setBounds(331, 151, 49, 14);
+		tabInformation.add(labelFecha);
+		
+		labelHora = new JLabel(horaComp);
+		labelHora.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		labelHora.setBounds(393, 151, 49, 14);
+		tabInformation.add(labelHora);
 		contentPane.add(tabbedPane, BorderLayout.WEST);
+		
+		JLabel textoFecha2 = new JLabel("a las");
+		textoFecha2.setFont(new Font("Tahoma", Font.PLAIN, 8));
+		textoFecha2.setBounds(374, 151, 24, 14);
+		tabInformation.add(textoFecha2);
+		
+		JTextArea areaLU = new JTextArea(Integer.toString(studentData.getId()));
+		areaLU.setBounds(89, 6, 130, 22);
+		tabInformation.add(areaLU);
+		
+		JTextArea areaApellido = new JTextArea(studentData.getLastName());
+		areaApellido.setBounds(89, 41, 130, 22);
+		tabInformation.add(areaApellido);
+		
+		JTextArea areaNombre = new JTextArea(studentData.getFirstName());
+		areaNombre.setBounds(89, 80, 130, 22);
+		tabInformation.add(areaNombre);
+		
+		JTextArea areaMail = new JTextArea(studentData.getMail());
+		areaMail.setBounds(52, 114, 172, 22);
+		tabInformation.add(areaMail);
+		
+		JTextArea areaGit = new JTextArea(studentData.getGithubURL());
+		areaGit.setBounds(52, 144, 172, 22);
+		tabInformation.add(areaGit);
+		
+		
 	}
 }
--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,4 +1,6 @@
 package launcher;
+import gui.*;
+import entities.*;
 
 public class Launcher {
 	public static void main(String [] args) {
@@ -6,7 +8,9 @@ public class Launcher {
         //creating and showing this application's GUI.
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-            	
+            	Student ezequiel = new Student(12323, "Aguilar", "Ezequiel", "ezequielmaximiliano123@gmail.com", "https://github.com/EzeAguilar", "src/images/descarga.jpg");
+            	SimplePresentationScreen a = new SimplePresentationScreen(ezequiel);
+            	a.setVisible(true);
             }
         });
     }
